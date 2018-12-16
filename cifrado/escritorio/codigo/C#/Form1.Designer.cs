566cfac056690f219940959154937ef7b5d8256b
RamirezRuizDaniel <35438790+RamirezRuizDaniel@users.noreply.github.com>
2018-12-15 21:34:10 -0600

    Add files via upload

---
+++ cifrado/escritorio/codigo/C#/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace CifrarCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTIA = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textoCifradoTIA = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericGrupoTIA = new System.Windows.Forms.NumericUpDown();
+            this.panelTI = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textoCifradoTI = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericDespCesar = new System.Windows.Forms.NumericUpDown();
+            this.panelCesar = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textoCifradoCesar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textoPlano = new System.Windows.Forms.TextBox();
+            this.panelTIA.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericGrupoTIA)).BeginInit();
+            this.panelTI.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericDespCesar)).BeginInit();
+            this.panelCesar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTIA
+            // 
+            this.panelTIA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelTIA.Controls.Add(this.label9);
+            this.panelTIA.Controls.Add(this.textoCifradoTIA);
+            this.panelTIA.Controls.Add(this.label4);
+            this.panelTIA.Controls.Add(this.label5);
+            this.panelTIA.Controls.Add(this.numericGrupoTIA);
+            this.panelTIA.Location = new System.Drawing.Point(679, 189);
+            this.panelTIA.Margin = new System.Windows.Forms.Padding(4);
+            this.panelTIA.Name = "panelTIA";
+            this.panelTIA.Size = new System.Drawing.Size(321, 235);
+            this.panelTIA.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(14, 12);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(160, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Transposicion Inversa Agrupada";
+            // 
+            // textoCifradoTIA
+            // 
+            this.textoCifradoTIA.Location = new System.Drawing.Point(17, 118);
+            this.textoCifradoTIA.Margin = new System.Windows.Forms.Padding(4);
+            this.textoCifradoTIA.Multiline = true;
+            this.textoCifradoTIA.Name = "textoCifradoTIA";
+            this.textoCifradoTIA.Size = new System.Drawing.Size(285, 90);
+            this.textoCifradoTIA.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 98);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Texto Cifrado";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 44);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Grupo";
+            // 
+            // numericGrupoTIA
+            // 
+            this.numericGrupoTIA.Location = new System.Drawing.Point(83, 42);
+            this.numericGrupoTIA.Margin = new System.Windows.Forms.Padding(4);
+            this.numericGrupoTIA.Name = "numericGrupoTIA";
+            this.numericGrupoTIA.Size = new System.Drawing.Size(104, 20);
+            this.numericGrupoTIA.TabIndex = 4;
+            // 
+            // panelTI
+            // 
+            this.panelTI.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelTI.Controls.Add(this.label8);
+            this.panelTI.Controls.Add(this.textoCifradoTI);
+            this.panelTI.Controls.Add(this.label6);
+            this.panelTI.Location = new System.Drawing.Point(345, 189);
+            this.panelTI.Margin = new System.Windows.Forms.Padding(4);
+            this.panelTI.Name = "panelTI";
+            this.panelTI.Size = new System.Drawing.Size(321, 235);
+            this.panelTI.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 12);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(111, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Transposicion Inversa";
+            // 
+            // textoCifradoTI
+            // 
+            this.textoCifradoTI.Location = new System.Drawing.Point(19, 76);
+            this.textoCifradoTI.Margin = new System.Windows.Forms.Padding(4);
+            this.textoCifradoTI.Multiline = true;
+            this.textoCifradoTI.Name = "textoCifradoTI";
+            this.textoCifradoTI.Size = new System.Drawing.Size(285, 132);
+            this.textoCifradoTI.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 57);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Texto Cifrado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Texto Cifrado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Desplazamiento";
+            // 
+            // numericDespCesar
+            // 
+            this.numericDespCesar.Location = new System.Drawing.Point(148, 46);
+            this.numericDespCesar.Margin = new System.Windows.Forms.Padding(4);
+            this.numericDespCesar.Name = "numericDespCesar";
+            this.numericDespCesar.Size = new System.Drawing.Size(104, 20);
+            this.numericDespCesar.TabIndex = 0;
+            // 
+            // panelCesar
+            // 
+            this.panelCesar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelCesar.Controls.Add(this.label7);
+            this.panelCesar.Controls.Add(this.textoCifradoCesar);
+            this.panelCesar.Controls.Add(this.label3);
+            this.panelCesar.Controls.Add(this.label2);
+            this.panelCesar.Controls.Add(this.numericDespCesar);
+            this.panelCesar.Location = new System.Drawing.Point(14, 189);
+            this.panelCesar.Margin = new System.Windows.Forms.Padding(4);
+            this.panelCesar.Name = "panelCesar";
+            this.panelCesar.Size = new System.Drawing.Size(321, 235);
+            this.panelCesar.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Cesar";
+            // 
+            // textoCifradoCesar
+            // 
+            this.textoCifradoCesar.Location = new System.Drawing.Point(16, 119);
+            this.textoCifradoCesar.Margin = new System.Windows.Forms.Padding(4);
+            this.textoCifradoCesar.Multiline = true;
+            this.textoCifradoCesar.Name = "textoCifradoCesar";
+            this.textoCifradoCesar.Size = new System.Drawing.Size(285, 90);
+            this.textoCifradoCesar.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(215, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Texto Plano";
+            // 
+            // textoPlano
+            // 
+            this.textoPlano.Location = new System.Drawing.Point(219, 37);
+            this.textoPlano.Margin = new System.Windows.Forms.Padding(4);
+            this.textoPlano.Multiline = true;
+            this.textoPlano.Name = "textoPlano";
+            this.textoPlano.Size = new System.Drawing.Size(584, 117);
+            this.textoPlano.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1015, 443);
+            this.Controls.Add(this.panelTIA);
+            this.Controls.Add(this.panelTI);
+            this.Controls.Add(this.panelCesar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textoPlano);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelTIA.ResumeLayout(false);
+            this.panelTIA.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericGrupoTIA)).EndInit();
+            this.panelTI.ResumeLayout(false);
+            this.panelTI.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericDespCesar)).EndInit();
+            this.panelCesar.ResumeLayout(false);
+            this.panelCesar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTIA;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textoCifradoTIA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericGrupoTIA;
+        private System.Windows.Forms.Panel panelTI;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textoCifradoTI;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericDespCesar;
+        private System.Windows.Forms.Panel panelCesar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textoCifradoCesar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textoPlano;
+
+    }
+}
+
